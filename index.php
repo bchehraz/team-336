782438bb2a0da9e1f2bc3037c5a055cfdb01fb21
zenitram56 <bramartinez@csumb.edu>
2017-04-13 04:29:48 +0000

    added basic html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Project</title>
+        <style>
+            @import url("css/team.css");
+        </style>
+    </head>
+    <body>
+        <h1>Music Search</h1>
+        
+        <form>
+            
+            Search Music: <input type="text" name="name"/>
+      Search Filters:<select name="Search Music">
+                       <option value="">Select One</option>
+                       <option value="artist">Artist</option>
+                       <option value="song">Artist</option>
+                       <option value="genre">Artist</option>
+                     </select>
+                
+                     <input type="submit" value="Search" />
+            
+            
+            
+        </form>
+        <br />
+        <hr>
+        <br />
+    </body>
+</html>
